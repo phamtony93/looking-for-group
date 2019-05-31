3e0bf5a277ccc6561f7fd24a7c0503c863f1ece5
phamtony93 <40592953+phamtony93@users.noreply.github.com>
2019-05-31 00:57:23 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,151 @@
+var express = require('express');
+var passport = require('passport');
+var cookieParser = require('cookie-parser')
+var FacebookStrategy = require('passport-facebook').Strategy
+var session = require('express-session')
+var config = require('./config')
+//var mysql = require('./dbcon.js');
+
+//Passport session setup
+passport.serializeUser(function(user, done) {
+	done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+	done(null, obj);
+});
+
+// Use the FacebookStrategy within Passport.
+
+passport.use(new FacebookStrategy({
+    clientID: config.facebook_api_key,
+    clientSecret:config.facebook_api_secret ,
+    callbackURL: config.callback_url
+  },
+  function(accessToken, refreshToken, profile, done) {
+    process.nextTick(function () {
+      //Check whether the User exists or not using profile.id
+      if(config.use_database) {
+        // if sets to true
+        pool.query("SELECT * from user_info where user_id="+profile.id, (err,rows) => {
+          if(err) throw err;
+          if(rows && rows.length === 0) {
+              console.log("There is no such user, adding now");
+              pool.query("INSERT into user_info(user_id,user_name) VALUES('"+profile.id+"','"+profile.username+"')");
+          } else {
+              console.log("User already exists in database");
+          }
+        });
+      }
+      return done(null, profile);
+    });
+  }
+));
+
+
+
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
+
+app.use(express.static(__dirname+'/public'));
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.use(cookieParser());
+app.set('port',33335);
+//app.set('mysql', mysql);
+
+app.use(session({ secret: 'keyboard cat',
+					key: 'sid',
+				resave: true,
+				saveUninitialized: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+
+app.get('/',function(req,res){
+	res.render('login');
+});
+
+app.get('/account', ensureAuthenticated, function(req, res){
+	res.render('account', { user: req.user });
+  });
+  
+  app.get('/auth/facebook', passport.authenticate('facebook',{scope:'email'}));
+  
+  
+  app.get('/auth/facebook/callback',
+	passport.authenticate('facebook', { successRedirect : '/', failureRedirect: '/login' }),
+	function(req, res) {
+	  res.redirect('/');
+	});
+  
+  app.get('/logout', function(req, res){
+	req.logout();
+	res.redirect('/');
+  });
+  
+  
+  function ensureAuthenticated(req, res, next) {
+	if (req.isAuthenticated()) { return next(); }
+	res.redirect('/login')
+  }
+
+// app.use('/actors', require('./actors.js'));
+
+// //app.get('/actors',function(req,res){
+// //	res.render('actors');
+// //});
+
+// app.use('/movies', require('./movies.js'));
+
+// //app.get('/movies',function(req,res){
+// //	res.render('movies');
+// //});
+
+// app.use('/genres', require('./genres.js'));
+
+// //app.get('/genres',function(req,res){
+// //	res.render('genres');
+// //});
+
+// app.use('/ratings', require('./ratings.js'));
+
+// //app.get('/ratings',function(req,res){
+// //	res.render('ratings');
+// //});
+
+// app.use('/producers', require('./producers.js'));
+
+// //app.get('/producers',function(req,res){
+// //	res.render('producers');
+// //});
+
+// app.use('/rating-agencies', require('./ratingAgencies.js'));
+
+// //app.get('/rating-agencies',function(req,res){
+// //	res.render('rating-agencies');
+// //});
+
+app.use(function(req,res){
+	res.status(404);
+	res.render('404');
+});
+
+app.use(function(err,req,res,next){
+	console.log(err.stack);
+	res.type('plain/text');
+	res.status(500);
+	res.render('500');
+});
+
+app.listen(app.get('port'),function(){
+	console.log('Express started on http://localhost:' + app.get('port') + '; press CTRL + C to terminate')
+});
---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	facebook_api_key: "1286461304862703",
+	facebook_api_secret: "4d0330ea1f9a682a9337ea049b50550a",
+	callback_url: "http://localhost:3000/auth/facebook/callback",
+	use_databse: false,
+	host: "localhost",
+	username: "root",
+	password: "",
+	databse: ""
+};
